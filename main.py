15dcd4d68f66b3f3bfe9fcef122d1383a4d73c02
Maksim <semimaks@mail.ru>
2022-03-16 20:18:02 +0300

    Базовая страница-пример работы folium с flask

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# python main.py
+# http://127.0.0.1:5000/
+
+from flask import Flask
+import folium
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
