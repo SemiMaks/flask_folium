03f75e6b96107b5c97b07695245d563dffe77f58
Maksim <semimaks@mail.ru>
2022-03-24 19:19:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,8 +10,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    start_coords = (55.5940700, 37.5160300)
+    tooltip = "Нажми меня!"
+    folium_map = folium.Map(location=start_coords, tiles="Stamen Terrain", zoom_start=9)
+
+    folium.Marker(
+        location=[55.6240700, 37.5160300],
+        popup="<i>Москва</i>", tooltip=tooltip,
+        icon=folium.Icon(color="green")
+    ).add_to(folium_map)
+
+    folium.Marker(
+        location=[55.6140700, 37.5160300],
+        popup="Метка", tooltip=tooltip,
+        icon=folium.Icon(color="red", icon="info-sign"),
+    ).add_to(folium_map)
+
+    folium.Marker(
+        location=[55.6040700, 37.5160300],
+        popup="Moscow",
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(folium_map)
+
+    folium.Marker(
+        location=[55.5940700, 37.5160300],
+        popup="<i>Москва</i>", tooltip=tooltip,
+    ).add_to(folium_map)
+
+
+
+
+
     return folium_map._repr_html_()
 
 
